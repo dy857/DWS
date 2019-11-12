a55ecfcc544daaa89a3a303ad497083f2e93ce03
feuziq47 <feuziq47@gmail.com>
2019-11-12 13:39:33 +0900

    turn yellow and sec_set

--- DWS.h
+++ DWS.h
@@ -6,6 +6,7 @@
 #include <time.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include<termios.h>
 
 #define SEC_SET 1
 #define HOUR_SET 2
@@ -20,8 +21,8 @@ typedef struct alarm_information{
 	int min;
 } alarm_info;
 
-
-
+pthread_t time_thread;
+int flag;
 char btn;
 time_t current_time;
 pthread_mutex_t mutex;
@@ -49,4 +50,18 @@ void turn_yellow();
 
 void btn_input();
 
+int getch()
+{
+    int c;
+    struct termios oldattr, newattr;
 
+    tcgetattr(STDIN_FILENO, &oldattr);           // 현재 터미널 설정 읽음
+    newattr = oldattr;
+    newattr.c_lflag &= ~(ICANON | ECHO);         // CANONICAL과 ECHO 끔
+    newattr.c_cc[VMIN] = 1;                      // 최소 입력 문자 수를 1로 설정
+    newattr.c_cc[VTIME] = 0;                     // 최소 읽기 대기 시간을 0으로 설정
+    tcsetattr(STDIN_FILENO, TCSANOW, &newattr);  // 터미널에 설정 입력
+    c = getchar();                               // 키보드 입력 읽음
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);  // 원래의 설정으로 복구
+    return c;
+}
--- Main.c
+++ Main.c
@@ -1,45 +1,111 @@
 #include "DWS.h"
 
 void* time_routine(void *arg){
-	pthread_t tid;
-	tid=pthread_self();
+	//pthread_t tid;
+	//tid=pthread_self();
 	while(1){
 		current_time =time(NULL);
 		struct tm current_tm=*localtime(&current_time);
 		system("clear");
 		pthread_mutex_lock(&mutex);
-		printf("현재시간: %d-%d-%d %d:%d:%d\n ",current_tm.tm_year+1900,current_tm.tm_mon+1,current_tm.tm_mday,current_tm.tm_hour,current_tm.tm_min,current_tm.tm_sec);
+		printf("현재시간: %d-%d-%d %d:%d:%d\n",current_tm.tm_year+1900,current_tm.tm_mon+1,current_tm.tm_mday,current_tm.tm_hour,current_tm.tm_min,current_tm.tm_sec);
 		pthread_mutex_unlock(&mutex);
 		sleep(1);
 	}
 	
 }
-
+void* sec_set_routine(){
+	while(1){
+		current_time =time(NULL);
+		struct tm current_tm=*localtime(&current_time);
+		system("clear");
+		pthread_mutex_lock(&mutex);
+		printf("현재시간: %d-%d-%d %d:%d:%c[4m%d%c[0m\n",current_tm.tm_year+1900,current_tm.tm_mon+1,current_tm.tm_mday,current_tm.tm_hour,current_tm.tm_min,27,current_tm.tm_sec,27);
+		pthread_mutex_unlock(&mutex);
+		sleep(1);
+	}
+}
+void* hour_set_routine(){
+	while(1){
+		current_time=time(NULL);
+		struct tm current_tm=*localtime(&current_time);
+		system("clear");
+		pthread_mutex_lock(&mutex);
+		printf("현재시간: %d-%d-%d %c[4m%d%c[0m:%d:%d\n",current_tm.tm_year+1900,current_tm.tm_mon+1,current_tm.tm_mday,27,current_tm.tm_hour,27,current_tm.tm_min,current_tm.tm_sec);
+		pthread_mutex_unlock(&mutex);
+		sleep(1);
+	}
+}
+void* min_set_routine(){
+	while(1){
+		current_time=time(NULL);
+		struct tm current_tm=*localtime(&current_time);
+		system("clear");
+		pthread_mutex_lock(&mutex);
+		printf("현재시간: %d-%d-%d %d:%c[4m%d%c[0m:%d\n",current_tm.tm_year+1900,current_tm.tm_mon+1,current_tm.tm_mday,current_tm.tm_hour,27,current_tm.tm_min,27,current_tm.tm_sec);
+		pthread_mutex_unlock(&mutex);
+		sleep(1);
+	}
+}
+void* year_set_routine(){
+	while(1){
+		current_time=time(NULL);
+		struct tm current_tm=*localtime(&current_time);
+		system("clear");
+		pthread_mutex_lock(&mutex);
+		printf("현재시간: %c[4m%d%c[0m-%d-%d %d:%d:%d\n",27,current_tm.tm_year+1900,27,current_tm.tm_mon+1,current_tm.tm_mday,current_tm.tm_hour,current_tm.tm_min,current_tm.tm_sec);
+		pthread_mutex_unlock(&mutex);
+		sleep(1);
+	}
+}
+void* year_set_routine(){
+	while(1){
+		current_time=time(NULL);
+		struct tm current_tm=*localtime(&current_time);
+		system("clear");
+		pthread_mutex_lock(&mutex);
+		printf("현재시간: %c[4m%d%c[0m-%d-%d %d:%d:%d\n",27,current_tm.tm_year+1900,27,current_tm.tm_mon+1,current_tm.tm_mday,current_tm.tm_hour,current_tm.tm_min,current_tm.tm_sec);
+		pthread_mutex_unlock(&mutex);
+		sleep(1);
+	}
+}
 void turn_yellow(){
-	printf("\033[1;33m");
-	fflush(stdout);
+	printf("%c[1;33m",27);
+	sleep(5);
+	system("reset");
+}
+void sec_set(void* arg){
+	pthread_create(&time_thread,NULL,sec_set_routine,NULL);
+	
+	while(1){
+		btn=getch();
+		if(btn=='a'){
+			printf("ddd\n");
+		}else if(btn=='c'){
+			//alarm_mode();
+		}else if(btn=='d'){
+			turn_yellow();
+		}
+	}
+
 }
 void main(){
-	pthread_t time_thread;
+	
 	
 	pthread_create(&time_thread,NULL,time_routine,NULL);
 	
 	pthread_mutex_init(&mutex,NULL);
 
-	btn=getchar();
+	
 	while(1){
-		switch(btn){
-			case 'a':
-				//sec_set();
-				break;
-			case 'c':
-				//alarm_mode();
-				break;
-			case 'd':
-				turn_yellow();
-				break;
-			default:
-				exit(1);
+		btn=getch();
+		if(btn=='a'){
+			pthread_cancel(time_thread);
+			sec_set(&time_thread);
+		}else if(btn=='c'){
+			//alarm_mode();
+		}else if(btn=='d'){
+			turn_yellow();
 		}
 	}
 	pthread_mutex_destroy(&mutex);
