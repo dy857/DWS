a4be343fd87c2c20c39b95acaef9e9d7ab566a27
KimDoYeon <dy857@naver.com>
2019-11-14 16:30:03 +0900

    separation

--- DWS.h
+++ DWS.h
@@ -31,80 +31,22 @@ typedef struct alarm_information{
 	int min;
 }alarm_info;
 
-int mo_info=0;
-int st_info=0;
+int mo_info;
+int st_info;
 char btn;
 time_t current_time;
 
-void time_mode()
-{
-	btn=' ';
-	mo_info = TIME_MODE;
-	st_info = 0;
-}
-
-void sec_set()
-{
-	btn=' ';
-	st_info = SEC_SET;
-	//깜빡깜빡
-	printf("초 바꾸는 중");
-}
-
-void hour_set()
-{
-	btn=' ';
-	st_info = HOUR_SET;
-	//깜빡깜빡
-	printf("시 바꾸는 중");
-	
-}
-
-void minute_set()
-{
-	btn=' ';
-	st_info = MINUTE_SET;
-	//깜빡깜빡
-	printf("분 바꾸는 중");
-}
-
-void year_set()
-{
-	btn=' ';
-	st_info = YEAR_SET;
-	//깜빡깜빡
-	printf("년 바꾸는 중");
-}
-
-void month_set()
-{
-	btn=' ';
-	st_info = MONTH_SET;
-	//깜빡깜빡
-	printf("달 바꾸는 중");
-}
-
-void day_set()
-{
-	btn=' ';
-	st_info = DAY_SET;
-	//깜빡깜빡
-	printf("일 바꾸는 중");
-}
-
+void time_mode();
+void sec_set();
+void hour_set();
+void minute_set();
+void year_set();
+void month_set();
+void day_set();
 void plus_one(int st_info);
 void set_min(int st_info);
-
-void alarm_mode()
-{
-	btn=' ';
-	mo_info = ALARM_MODE;
-}
-void alarm_indicator()
-{
-	btn=' ';
-	printf("indicator");
-}
+void alarm_mode();
+void alarm_indicator();
 void lap_time();
 void stop();
 void start();
@@ -117,45 +59,6 @@ void turn_yellow();
 
 void btn_input();
 
-int getch()
-{
-	int c;
-    	struct termios oldattr, newattr;
+int getch();
 
-    	tcgetattr(STDIN_FILENO, &oldattr);           // 현재 터미널 설정 읽음
-   	newattr = oldattr;
-   	newattr.c_lflag &= ~(ICANON | ECHO);         // CANONICAL과 ECHO 끔
-    	newattr.c_cc[VMIN] = 1;                      // 최소 입력 문자 수를 1로 설정
-    	newattr.c_cc[VTIME] = 0;                     // 최소 읽기 대기 시간을 0으로 설정
-    	tcsetattr(STDIN_FILENO, TCSANOW, &newattr);  // 터미널에 설정 입력
-    	c = getchar();                               // 키보드 입력 읽음
-    	tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);  // 원래의 설정으로 복구
-    	return c;
-}
-
-int kbhit(void)
-{
-    	struct termios oldt, newt;
-    	int ch;
-    	int oldf;
- 	
- 	tcgetattr(STDIN_FILENO, &oldt);
-    	newt = oldt;
-    	newt.c_lflag &= ~(ICANON | ECHO);
-    	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-    	oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-    	fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
- 
-    	ch = getchar();
- 
-    	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-    	fcntl(STDIN_FILENO, F_SETFL, oldf);
- 
-    	if(ch != EOF)
-    	{
-    		ungetc(ch, stdin);
-    		return 1;
-    	}
- 
-    	return 0;
-}
+int kbhit(void);
--- Main.c
+++ Main.c
@@ -1,26 +1,8 @@
 #include "DWS.h"
 
-
-void turn_yellow(){
-	printf("%c[1;33m",27);
-	sleep(2);
-	system("reset");
-}
-
-void current(){
-	current_time =time(NULL);
-	struct tm current_tm=*localtime(&current_time);
-	system("clear");
-	printf("현재시간: %d-%d-%d %d:%d:%d\n",current_tm.tm_year+1900,current_tm.tm_mon+1,current_tm.tm_mday,current_tm.tm_hour,current_tm.tm_min,current_tm.tm_sec);
-}
-void turn_on(){
-	
-}
 void main(){
 	//
 	alarm_info alarm;
-	alarm.alarm_power = false;
-	alarm.info = false;
 	// 초기 시간 설정 바꾸기
 	current_time =time(NULL);
 	struct tm current_tm=*localtime(&current_time);
